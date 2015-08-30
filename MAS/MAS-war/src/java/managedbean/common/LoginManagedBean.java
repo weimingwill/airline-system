0622bd7518c246101f0cadd27d634fdaeac80e32
Liu Xin <lewisxin718@gmail.com>
2015-08-30 18:54:10 +0800

    Modify LoginManagedBean

--- MAS/MAS-war/src/java/managedbean/common/LoginManagedBean.java
+++ MAS/MAS-war/src/java/managedbean/common/LoginManagedBean.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package managedbean;
+package managedbean.common;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import mas.common.session.SystemUserSessionLocal;
 
 /**
  *
@@ -15,11 +17,48 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class LoginManagedBean {
-
+    @EJB
+    private SystemUserSessionLocal systemUserSession;
+    private String username;
+    private String password;
+    
+    
     /**
      * Creates a new instance of LoginManagedBean
      */
     public LoginManagedBean() {
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public void varifyUserLogin(String userName, String password){
+        systemUserSession.verifySystemUserPassword(userName, password);
     }
     
 }
