26a7c8a68d8317d7a43e907f270445e0491351df
Weiming <wingalong@gmail.com>
2015-09-15 20:33:48 +0800

    create ais entity Cabin Class

---
+++ MAS/MAS-ejb/src/java/ams/ais/entity/CabinClass.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ams.ais.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author winga_000
+ */
+@Entity
+public class CabinClass implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cabinClassId;
+    private String name;
+    private int seatQty;
+    
+    
+    public Long getCabinClassId() {
+        return cabinClassId;
+    }
+
+    public void setCabinClassId(Long cabinClassId) {
+        this.cabinClassId = cabinClassId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeatQty() {
+        return seatQty;
+    }
+
+    public void setSeatQty(int seatQty) {
+        this.seatQty = seatQty;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (cabinClassId != null ? cabinClassId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the cabinClassId fields are not set
+        if (!(object instanceof CabinClass)) {
+            return false;
+        }
+        CabinClass other = (CabinClass) object;
+        if ((this.cabinClassId == null && other.cabinClassId != null) || (this.cabinClassId != null && !this.cabinClassId.equals(other.cabinClassId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ams.ais.entity.CabinClass[ cabinClassId=" + cabinClassId + " ]";
+    }
+    
+}
