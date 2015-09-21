116c0146a727b3df77d2bd3f0b35648767ecb69c
Bowen Wei <weibowen1992@gmail.com>
2015-09-21 11:19:56 +0800

    add cabin class function

--- MAS/MAS-ejb/src/java/ams/ais/session/CabinClassSession.java
+++ MAS/MAS-ejb/src/java/ams/ais/session/CabinClassSession.java
@@ -5,15 +5,68 @@
  */
 package ams.ais.session;
 
+import ams.ais.entity.CabinClass;
+import ams.ais.entity.TicketFamily;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
- * @author winga_000
+ * @author Bowen
  */
 @Stateless
 public class CabinClassSession implements CabinClassSessionLocal {
 
+    @EJB
+    private TicketFamilySessionLocal ticketFamilySession;
+    
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    
+    @Override
+    public void createCabinClass(String name, String type) {
+        CabinClass cabinClass = new CabinClass();
+        cabinClass.create(type,name);
+        entityManager.persist(cabinClass);
+    }
+
+    @Override
+    public List<CabinClass> getAllCabinClass() {
+        Query query = entityManager.createQuery("SELECT c FROM CabinClass c");
+        return query.getResultList();
+        
+
+    }
+
+    @Override
+    public void deleteCabinClass(String name) {
+        
+        CabinClass cabinclass = getCabinClassByName(name);
+        cabinclass.setDeleted(true);
+        entityManager.merge(cabinclass);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CabinClass getCabinClassByName(String name) {
+        Query query = entityManager.createQuery("SELECT c FROM CabinClass c WHERE c.name = :inCabinClassName");
+        query.setParameter("inCabinClassName", name);
+        CabinClass cabinclass = null;
+        try {
+            cabinclass = (CabinClass) query.getSingleResult();
+        } catch (NoResultException ex) {
+            cabinclass = null;
+        }
+        return cabinclass;
+        
+        //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- MAS/MAS-ejb/src/java/ams/ais/session/CabinClassSessionLocal.java
+++ MAS/MAS-ejb/src/java/ams/ais/session/CabinClassSessionLocal.java
@@ -5,13 +5,21 @@
  */
 package ams.ais.session;
 
+import ams.ais.entity.CabinClass;
+import ams.ais.entity.TicketFamily;
+import java.util.List;
 import javax.ejb.Local;
 
 /**
  *
- * @author winga_000
+ * @author Bowen
  */
 @Local
 public interface CabinClassSessionLocal {
     
+   public List<CabinClass> getAllCabinClass();
+   public void createCabinClass(String name, String type);
+   public void deleteCabinClass(String name);
+   public CabinClass getCabinClassByName(String name);
+   
 }
