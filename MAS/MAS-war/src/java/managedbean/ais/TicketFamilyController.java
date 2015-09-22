344d2c87752c6f0f269c90bce49b516fc26077e2
Bowen Wei <weibowen1992@gmail.com>
2015-09-22 21:48:36 +0800

    Product design cabinclass, rule controller update

---
+++ MAS/MAS-war/src/java/managedbean/ais/TicketFamilyController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbean.ais;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Bowen
+ */
+@Named(value = "ticketFamilyController")
+@RequestScoped
+public class TicketFamilyController {
+
+    /**
+     * Creates a new instance of TicketFamilyController
+     */
+    public TicketFamilyController() {
+    }
+    
+}
