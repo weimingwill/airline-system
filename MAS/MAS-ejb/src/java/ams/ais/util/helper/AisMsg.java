63cc00b16650e5e5376ffe8adb675d22bfa488e9
Bowen Wei <weibowen1992@gmail.com>
2015-09-22 21:46:46 +0800

    Update ais error message for rule session

--- MAS/MAS-ejb/src/java/ams/ais/util/helper/AisMsg.java
+++ MAS/MAS-ejb/src/java/ams/ais/util/helper/AisMsg.java
@@ -15,4 +15,6 @@ public class AisMsg {
     public static final String EXIST_SUCH_CABIN_CLASS_TYPE_ERROR = "Cabin class type already exists";
     public static final String EXIST_SUCH_CABIN_CLASS_NAME_ERROR = "Cabin class name already exists";
     public static final String NO_SUCH_CABIN_CLASS_ERROR = "Cabin class doesn't exist";
+    public static final String EXIST_SUCH_RULE_ERROR = "Rule already exists";
+    public static final String NO_SUCH_RULE_ERROR = "Rule doesn't exists";
 }
