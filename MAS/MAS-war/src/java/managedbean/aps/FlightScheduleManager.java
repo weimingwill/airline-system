6df2c685323fdd2fa60d64e28a1c45a8f391376d
Weiming <wingalong@gmail.com>
2015-10-10 22:35:38 +0800

    Add filter flight fliter template.

---
+++ MAS/MAS-war/src/java/managedbean/aps/FlightScheduleManager.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbean.aps;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author winga_000
+ */
+@Named(value = "flightScheduleManager")
+@SessionScoped
+public class FlightScheduleManager implements Serializable {
+
+    /**
+     * Creates a new instance of FlightScheduleManager
+     */
+    public FlightScheduleManager() {
+    }
+    
+}
