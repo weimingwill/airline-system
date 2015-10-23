4fc8518329b2eec5f2a0118cd5c8ccc1bd0a0b92
Liu Chuning <pomeloecho@gmail.com>
2015-10-23 21:04:52 +0800

    Create entities: Luggage and LuggageTag

--- MAS/MAS-ejb/src/java/ams/ars_crm/entity/Customer.java
+++ MAS/MAS-ejb/src/java/ams/ars_crm/entity/Customer.java
@@ -15,6 +15,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -24,6 +26,7 @@ import javax.persistence.TemporalType;
  * @author Tongtong
  */
 @Entity
+@Inheritance(strategy=InheritanceType.JOINED)
 public class Customer implements Serializable {
 
     private static final long serialVersionUID = 1L;
